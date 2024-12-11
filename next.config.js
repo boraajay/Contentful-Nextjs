167561915d6f32f42d8550f79703888270c0b922
boraajay <165030977+boraajay@users.noreply.github.com>
2024-12-11 18:36:37 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    reactStrictMode: true,
+    i18n: {
+      locales: ['en-US', 'hi'], // List all supported locales
+      defaultLocale: 'en-US',      // Set the default locale
+    },
+};
