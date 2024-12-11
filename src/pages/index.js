5d8aa5c38ed5962b1157ef76d08de9f9a2165dd8
boraajay <165030977+boraajay@users.noreply.github.com>
2024-12-11 19:09:07 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,32 @@ import Hero from '../components/HeroBanner';
 import client from '@/lib/contentful'; 
 
 const Main = ({ homePage, heroBanner }) => {
+  const { locale, asPath, push } = useRouter();
+  const changeLanguage = (newLocale) => {
+      push(asPath, asPath, { locale: newLocale });
+    };
   return (
     <div className="container">
+      <p>Current Language: {locale}</p>
+      <button onClick={() => changeLanguage('en-US')}>English</button>
+      <button onClick={() => changeLanguage('ar')}>Arabic</button>
       <Home homePage={homePage} />
       <Hero heroBanner={heroBanner} />
     </div>
   );
 };
 
-export async function getStaticProps() {
+export async function getStaticProps( {locale} ) {
   try {
 
     const resHome = await client.getEntries({
       content_type: 'homePage',  
+      locale,
     });
 
     const res = await client.getEntries({
       content_type: 'heroBanner',
+      locale,
     });
 
     const homePage = resHome.items[0];
