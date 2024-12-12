4f0ed296aa1d5a2a2e64f73f36a5e8cd178bf79b
boraajay <165030977+boraajay@users.noreply.github.com>
2024-12-12 17:28:03 +0530

    Update index.js

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,12 +1,18 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';  // Import for rendering RichText
 
 const Home = ({homePage}) =>{
+  console.log(homePage?.fields?.titleList);
+  console.log(homePage?.fields?.titleApperance);
   return(  
         <div>
             <h1 id='homePageTitle' className="title">{homePage?.fields?.homePageTitle}</h1>
             <div id='homePagedescription' className="description">
                 {documentToReactComponents(homePage?.fields?.homePageDescription)}
             </div>
+            {homePage?.fields?.titleList?.map((list) => (
+              <p key={list?.sys?.space?.id}>{list}</p>
+            )
+            )}
         </div>)
 };
 
